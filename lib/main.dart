bd9c4605a0893d9073d35b84c49e2e3210e9257b
Sanjk <elhassanmohammed123@gmail.com>
2026-05-17 09:58:08 +0800

    added firebase initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+runApp(const MyApp());
 
