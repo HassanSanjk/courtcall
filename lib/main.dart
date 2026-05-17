87936d386782604c84532bef3f771f8793b1728c
Sanjk <elhassanmohammed123@gmail.com>
2026-05-17 23:07:14 +0800

    fixed main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
-runApp(const MyApp());
 
